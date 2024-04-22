75878ae6f9a0244cc57aa33c78536c3771666ae5
zheng-lv <82482684+zheng-lv@users.noreply.github.com>
2024-04-22 10:39:54 +0800

    Update main.cpp

--- example/simple_main/main.cpp
+++ example/simple_main/main.cpp
@@ -10,17 +10,21 @@ using std::vector;
 
 #include <CppDNN/DeepNueralNetwork.h>
 
-
 int main()
 {
+    // 定义一个 DeepNueralNetwork 类型的对象 dnn，表示神经网络
     DeepNueralNetwork dnn;
+    // 从指定路径读取使用 Keras 训练的神经网络参数
     dnn.ReadFromKeras("/home/nader/workspace/github/CppDNN/example/keras_simple/simple.txt");
+    // 创建一个 5x1 的矩阵 input，用于输入数据
     MatrixXd input(5,1);
     input(0,0) = 1;
     input(1,0) = 2;
     input(2,0) = 1;
     input(3,0) = 2;
     input(4,0) = 1;
+    // 调用 Calculate 方法进行神经网络的计算
     dnn.Calculate(input);
+    // 输出神经网络的计算结果
     cout<<dnn.mOutput<<std::endl;
 }
